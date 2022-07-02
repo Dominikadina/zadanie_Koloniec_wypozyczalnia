725fd12eda7436afb6cbc7726be7873db6caa1f9
Dominika <dominika.d.gacek@gmail.com>
2022-07-02 12:12:42 +0200

    pirwszy

---
+++ src/main/java/pl/sda/arppl4/hibernate_wypozyczalnia/model/Samochod.java
@@ -0,0 +1,25 @@
+package pl.sda.arppl4.hibernate_wypozyczalnia.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Samochod {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String nazwa;
+    private String model;
+    private LocalDate data;
+    private int iloscPsazerow;
+    @Enumerated(EnumType.STRING)
+    private TypNadwozia typNadwozia;
+}
---
+++ src/main/java/pl/sda/arppl4/hibernate_wypozyczalnia/model/TypNadwozia.java
@@ -0,0 +1,7 @@
+package pl.sda.arppl4.hibernate_wypozyczalnia.model;
+
+public enum TypNadwozia {
+    SEDAN,
+    CABRIO,
+    SUV;
+}
